79a7f3583a9342183b4fda1bdea99ea70790e1dd
MarianaGFT <48689127+MarianaGFT@users.noreply.github.com>
2020-03-26 00:44:00 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState} from 'react';
+import { Card,Cascader, Row, Col } from 'antd';
+import "antd/dist/antd.css";
+
+const { Meta } = Card;
 
 function App() {
+
+  const options= [
+    {
+      value: 'beagle',
+      label: 'Beagle',
+    },
+    {
+    value: 'husky',
+    label: 'Husky',
+    },
+    {
+      value: 'chihuahua',
+      label: 'Chihuahua',
+    },
+    {
+      value: 'pug',
+      label: 'Pug',
+    },
+    {
+      value: 'shiba',
+      label: 'Shiba',
+    },
+    {
+      value: 'labrador',
+      label: 'Labrador',
+    },
+    {
+      value: 'akita',
+      label: 'Akita',
+    },
+    {
+      value: 'malamute',
+      label: 'Malamute',
+    },
+    {
+      value: 'papillon',
+      label: 'Papillon',
+    },
+    {
+      value: 'mexicanhairless',
+      label: 'Mexicanhairless',
+    },
+    {
+      value: 'appenzeller',
+      label: 'Appenzeller',
+    }
+  ]
+
+  const[dogs,setDogs]=useState([]);
+  const [dogType,setDogType]=useState("beagle");
+
+   const obtenerDatosDeLaAPI = async ()=>{
+   const result= await  fetch(`https://dog.ceo/api/breed/${dogType}/images`);
+   const json= await result.json();
+   setDogs(json.message);
+  } 
+
+  function onChange(value){
+    if(value[0])
+    setDogType(value[0]);
+  }
+
+  useEffect(()=>{
+    obtenerDatosDeLaAPI()
+  },[dogType]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Cascader  options={options} onChange={onChange}/>
+    <Row>
+    {dogs.map((dog,index)=>(
+    <Col xs={24} md={12} lg={8}   key={index}>
+     <Card
+     hoverable
+     style={{margin:10}}
+     cover={<img alt="puppy" src={dog} style={{height:300,objectFit:'cover'}}/>}
+   >
+   <Meta title={dogType.toUpperCase()} />
+   </Card>
+   </Col>
+    ))}
+    </Row>
+  </div>
   );
 }
 
